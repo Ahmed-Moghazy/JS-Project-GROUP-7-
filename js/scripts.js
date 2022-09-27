38f0068f8d9406b0cba1961f470c098a7cfec647
erinikamel <erinikamel1993@gmail.com>
2022-09-27 22:46:20 +0200

    cart calculations

--- js/scripts.js
+++ js/scripts.js
@@ -49,7 +49,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
     });
   });
 });
-//==================================================Statr Home Page===========================================
+//==================================================Start Home Page===========================================
 // stop nav
 let nav = document.querySelector(".header-nav");
 let scrol;
@@ -159,9 +159,6 @@ let alertTxtD = document.querySelector(".message-deleted");
 //   cartContent.insertAdjacentHTML("beforeend", data);
 // }
 
-
-
-
 for (let i = 0; i < addBtn.length; i++) {
   if (addBtn[i]) {
     addBtn[i].addEventListener("click", function () {
@@ -180,7 +177,7 @@ for (let i = 0; i < addBtn.length; i++) {
 for (let i = 0; i < deleteBtn.length; i++) {
   if (deleteBtn[i]) {
     deleteBtn[i].addEventListener("click", function () {
-      if (counterTxt[i].value > 0) {
+      if (counterTxt[i].value > 1) {
         counterTxt[i].value--;
         alertTxtD.style.display = "block";
         alertTxtD.innerHTML = `1 item deleted`;
@@ -188,103 +185,250 @@ for (let i = 0; i < deleteBtn.length; i++) {
           alertTxtD.style.display = "none";
         }, 2000);
       } else {
-        counterTxt[i].value = 0;
+        counterTxt[i].value = 1;
       }
     });
   }
 }
 //==================================================End Home Page===========================================
 //==================================================Statr Sign Up Page===========================================
-let fullName = document.querySelector("#full-name");
-let email = document.querySelector("#email");
-let password = document.querySelector("#password");
-let confirmPassword = document.querySelector("#confirm-password");
-let submit = document.querySelector("#submit");
-// Create An Array To Store Object Data
-let signupArray = [];
-// Check If Local Storag Has Data
-if (localStorage.getItem("UserInfo")) {
-  signupArray = JSON.parse(localStorage.getItem("UserInfo"));
-}
-// Tregger getUserDataFromLs Function
+// let fullName = document.querySelector("#full-name");
+// let email = document.querySelector("#email");
+// let password = document.querySelector("#password");
+// let confirmPassword = document.querySelector("#confirm-password");
+// let submit = document.querySelector("#submit");
+// // Create An Array To Store Object Data
+// let signupArray = [];
+// // Check If Local Storag Has Data
+// if (localStorage.getItem("UserInfo")) {
+//   signupArray = JSON.parse(localStorage.getItem("UserInfo"));
+// }
+// // Tregger getUserDataFromLs Function
 
-// Submit Event
-if (submit) {
-  submit.addEventListener("click", (e) => {
-    // e.preventDefault();
-    if (
-      (fullName.value !== "") &
-      (email.value !== "") &
-      (confirmPassword.value !== "") &
-      (password.value !== "")
-    ) {
-      if (password.value == confirmPassword.value) {
-        addUserDataToArray(fullName.value, email.value, password.value);
-        fullName.value = "";
-        email.value = "";
-        password.value = "";
-        confirmPassword.value = "";
-        window.location.assign("../pages/login.html");
-      } else {
-        confirmPassword.value = "";
-        alert("password not same");
-      }
-    }
-  });
-}
+// // Submit Event
+// if (submit) {
+//   submit.addEventListener("click", (e) => {
+//     // e.preventDefault();
+//     if (
+//       (fullName.value !== "") &
+//       (email.value !== "") &
+//       (confirmPassword.value !== "") &
+//       (password.value !== "")
+//     ) {
+//       if (password.value == confirmPassword.value) {
+//         addUserDataToArray(fullName.value, email.value, password.value);
+//         fullName.value = "";
+//         email.value = "";
+//         password.value = "";
+//         confirmPassword.value = "";
+//         window.location.assign("../pages/login.html");
+//       } else {
+//         confirmPassword.value = "";
+//         alert("password not same");
+//       }
+//     }
+//   });
+// }
 
-// Function To Add Object Of Data To Array Of Objects
-function addUserDataToArray(name, email, pass) {
-  //Object To Add To Array
-  const signupData = {
-    id: Date.now(),
-    fullname: name,
-    email: email,
-    password: pass,
-    login: false,
+// // Function To Add Object Of Data To Array Of Objects
+// function addUserDataToArray(name, email, pass) {
+//   //Object To Add To Array
+//   const signupData = {
+//     id: Date.now(),
+//     fullname: name,
+//     email: email,
+//     password: pass,
+//     login: false,
+//   };
+//   // Push Object To Array
+//   signupArray.push(signupData);
+//   // Add Data To Local Storage
+//   addSignupDataToLS(signupArray);
+// }
+// //Function To Add Signup Data To Local Storage
+// function addSignupDataToLS(array) {
+//   window.localStorage.setItem("UserInfo", JSON.stringify(array));
+// }
+// //==================================================End Sign Up Page===========================================
+// //==================================================Statr Log In Page===========================================
+
+// let loginEmail = document.querySelector("#login-email");
+// let loginPassword = document.querySelector("#login-password");
+// let loginSubmit = document.querySelector("#login-submit");
+
+// if (loginSubmit) {
+//   loginSubmit.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     if ((loginEmail !== "") & (loginPassword !== "")) {
+//       let d = getUserDataFromLS();
+//       if (d == true) {
+//         window.location.assign("../index.html");
+//       } else {
+//         alert("Invalid Email Or Password");
+//       }
+//     }
+//   });
+// }
+// function getUserDataFromLS() {
+//   let data = window.localStorage.getItem("UserInfo");
+//   if (data) {
+//     let loginData = JSON.parse(data);
+//     for (let i = 0; i < loginData.length; i++) {
+//       if (loginEmail.value == loginData[i].email) {
+//         if (loginPassword.value == loginData[i].password) {
+//           return true;
+//         }
+//       }
+//     }
+//     return false;
+//   }
+// }
+
+//==================================================End Log In Page===========================================
+
+//==================================================Cart Page Price calculations ===========================================
+
+// Declaring elements
+const cartCard = document.getElementsByClassName("cartCard");
+const plusBtns = document.getElementsByClassName("plusBtn");
+const minusBtns = document.getElementsByClassName("minusBtn");
+const quantity = document.getElementsByClassName("quantity");
+let price = document.getElementsByClassName("priceTxt");
+let trashBtns = document.getElementsByClassName("trashBin");
+let itemsNum = document.getElementById("itemsNum");
+let shippingfee = document.getElementById("shippingFee");
+let total = document.getElementsByClassName("total");
+let subtotal = document.getElementById("subtotal");
+let subTotal = 0;
+//Mapping number of items on load to "You have # items in your cart"
+itemsNum.innerText = trashBtns.length;
+
+// <-----------------------Functions----------------------->
+
+// Function to split currency and amount of product price
+let splitPrice = function (string) {
+  let amount = string.match(/[0-9]+([,.][0-9]+)?/);
+  let unit = string.replace(/[0-9]+([,.][0-9]+)?/, "");
+  if (amount && unit) {
+    return {
+      amount: +amount[0].replace(",", "."),
+      currency: unit,
+    };
+  }
+  return {
+    amount: +amount[0].replace(",", "."),
   };
-  // Push Object To Array
-  signupArray.push(signupData);
-  // Add Data To Local Storage
-  addSignupDataToLS(signupArray);
-}
-//Function To Add Signup Data To Local Storage
-function addSignupDataToLS(array) {
-  window.localStorage.setItem("UserInfo", JSON.stringify(array));
-}
-//==================================================End Sign Up Page===========================================
-//==================================================Statr Log In Page===========================================
+};
 
-let loginEmail = document.querySelector("#login-email");
-let loginPassword = document.querySelector("#login-password");
-let loginSubmit = document.querySelector("#login-submit");
-
-if (loginSubmit) {
-  loginSubmit.addEventListener("click", function (e) {
-    e.preventDefault();
-    if ((loginEmail !== "") & (loginPassword !== "")) {
-      let d = getUserDataFromLS();
-      if (d == true) {
-        window.location.assign("../index.html");
-      } else {
-        alert("Invalid Email Or Password");
-      }
-    }
-  });
+// Function to retrieve initial subtotal price of cart items
+function getInitialSubTotal() {
+  for (let i = 0; i < trashBtns.length; i++) {
+    const priceofOne = splitPrice(price[i].innerText).amount;
+    subTotal += priceofOne;
+    subtotal.innerText = `$${subTotal}`;
+  }
 }
-function getUserDataFromLS() {
-  let data = window.localStorage.getItem("UserInfo");
-  if (data) {
-    let loginData = JSON.parse(data);
-    for (let i = 0; i < loginData.length; i++) {
-      if (loginEmail.value == loginData[i].email) {
-        if (loginPassword.value == loginData[i].password) {
-          return true;
-        }
-      }
-    }
-    return false;
+getInitialSubTotal();
+console.log(cartCard);
+// Removing item when clicking on trashbin (delete)
+for (let i = 0; i < trashBtns.length; i++) {
+  if (trashBtns[i]) {
+    trashBtns[i].addEventListener("click", function (event) {
+      console.log(cartCard);
+      console.log(quantity.length);
+      // event.target.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.remove();
+      cartCard[i].remove();
+      console.log(cartCard);
+      console.log(quantity.length);
+      // Updating subtotal when an item is removed
+      let button = event.currentTarget;
+      let removedPrice = splitPrice(
+        button.previousElementSibling.innerText
+      ).amount;
+      subTotal -= removedPrice;
+      subtotal.innerText = `$${subTotal}`;
+      //Updating title of "You have # items in your cart"
+      itemsNum.innerText = trashBtns.length;
+      i -= i;
+    });
   }
 }
 
-//==================================================End Log In Page===========================================
+// Updating item price based on quantity input change (on change)
+for (let i = 0; i < cartCard.length; i++) {
+  const priceofOne = splitPrice(price[i].innerText).amount;
+  if (cartCard[i]) {
+    quantity[i].addEventListener("change", function () {
+      if (quantity[i].value <= 10) {
+        let itemsPrice = Number(`${this.value * priceofOne}`);
+        price[i].innerText = `$${itemsPrice}`;
+        subTotal += Number(priceofOne);
+        subtotal.innerText = `$${subTotal}`;
+      }
+    });
+  }
+}
+
+// Updating item price based on quantity input change (using plus button)
+for (let i = 0; i < plusBtns.length; i++) {
+  const priceofOne = splitPrice(price[i].innerText).amount;
+  if (plusBtns[i]) {
+    plusBtns[i].addEventListener("click", function (event) {
+      if (quantity[i].value < 10) {
+        quantity[i].value++;
+        let itemsPrice = Number(`${quantity[i].value * priceofOne}`);
+        price[i].innerText = `$${itemsPrice}`;
+        subTotal += Number(priceofOne);
+        subtotal.innerText = `$${subTotal}`;
+      }
+    });
+  }
+}
+
+// Updating item price based on quantity input change (using minus button)
+for (let i = 0; i < minusBtns.length; i++) {
+  const priceofOne = splitPrice(price[i].innerText).amount;
+  if (minusBtns[i]) {
+    minusBtns[i].addEventListener("click", function () {
+      if (quantity[i].value > 1) {
+        quantity[i].value--;
+        let itemsPrice = Number(`${quantity[i].value * priceofOne}`);
+        price[i].innerText = `$${itemsPrice}`;
+        subTotal -= Number(priceofOne);
+        subtotal.innerText = `$${subTotal}`;
+      }
+    });
+  }
+}
+
+// Removing item when clicking on trashbin (delete)
+
+// if (trashBtns[i]) {
+
+// for (let i = 0; i < trashBtns.length; i++) {
+// // let button = event.currentTarget;
+// // button.parentElement.parentElement.parentElement.parentElement.removeChild();
+// trashBtns.splice[i,1];
+// //Updating subtotal when an item is removed
+// let removedPrice = splitPrice(
+//   button.previousElementSibling.innerText
+// ).amount;
+// subTotal -= removedPrice;
+// subtotal.innerText = `$${subTotal}`;
+// //Updating title of "You have # items in your cart"
+// itemsNum.innerText = trashBtns.length;
+// }
+
+// Setting shipping fee
+let shippingFee = splitPrice(shippingfee.innerText).amount;
+shippingfee.innerText = `$${shippingFee}`;
+
+// Calculating total price of purchases
+function getTotalOfPurchase() {
+  for (let i = 0; i < total.length; i++) {
+    let totalPrice = `${subTotal + shippingFee}`;
+    total[i].innerText = `$${totalPrice}`;
+  }
+}
+
+getTotalOfPurchase();
